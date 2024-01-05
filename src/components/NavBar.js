00d75fd6056993a9562b483320d5989ccf82507d
Mikailalvares <mikailismail260@gmail.com>
2024-01-05 16:14:07 +0300

    updated classsName

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
@@ -8,12 +8,12 @@ function NavBar() {
         <h1>EduForge</h1>
       </Link>
       <form action="" id="search-form">
-        <input type="search" name="" id="search-input" placeholder='Search'/>
+        <input type="search" id="search-input" placeholder='Search' />
         <button type="submit">Submit</button>
       </form>
-      <i class="fa-regular fa-circle-user"></i>
+      <i className="fa-regular fa-circle-user"></i>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
